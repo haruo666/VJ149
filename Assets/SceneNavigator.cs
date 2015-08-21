22c0073ebeeb19c86fe85a6882c587262d03dda3
Haruo Fujiwara <haruo.fujiwara@icloud.com>
2015-08-21 16:22:54 +0900

    Updated scene navigator.

--- Assets/SceneNavigator.cs
+++ Assets/SceneNavigator.cs
@@ -15,7 +15,11 @@ public class SceneNavigator : MonoBehaviour {
 		} else if (Input.GetKeyDown (KeyCode.W)) {
 			Application.LoadLevel ("AudioWave3DLines");
 		} else if (Input.GetKeyDown (KeyCode.S)) {
-			Application.LoadLevel("SunBurst");
+			Application.LoadLevel ("SunBurst");
+		} else if (Input.GetKeyDown (KeyCode.A)) {
+			Application.LoadLevel ("AdStars");
+		} else if (Input.GetKeyDown (KeyCode.B)) {
+			Application.LoadLevel("AudioSpectrumBase2");
 		}
 
 		if (Input.GetKeyDown (KeyCode.F)) {
