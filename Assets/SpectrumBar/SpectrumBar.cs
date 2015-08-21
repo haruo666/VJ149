9c3daf9206c73624779b04bc46d165b1c7e4e554
nino <dev001@ninonote.com>
2015-08-21 16:23:28 +0900

    effects update

--- Assets/SpectrumBar/SpectrumBar.cs
+++ Assets/SpectrumBar/SpectrumBar.cs
@@ -33,7 +33,7 @@ public class SpectrumBar : MonoBehaviour
             }
 
             var vs = transform.localScale;
-            vs.y = 0.5f + scale * 80.0f;
+            vs.y = 0.2f + scale * 200.0f;
             transform.localScale = vs;
         }
     }
